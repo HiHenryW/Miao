be60cdb54da890fd11582f710447be436cf5eb21
Henry Wang <henry.h.wang93@gmail.com>
2020-07-30 00:32:51 -0400

    finish initial full stack setup

---
+++ root/back-end/src/index.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import models, { connectDb } from './models';
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  // don't forget to reference models here
+  return res.send('Root of directory reached!');
+});
+
+const willSeedDatabase = false;
+
+connectDb().then(async () => {
+  if (willSeedDatabase) {
+    await Promise.all([models.User.deleteMany({})]);
+
+    createUsers();
+  }
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Back-end server listening on port ${process.env.PORT}!`);
+  });
+});
+
+const createUsers = async () => {
+  const user = new models.User({
+    username: 'haihenry',
+  });
+
+  await user.save();
+};

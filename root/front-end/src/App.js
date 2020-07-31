5b6100fc55b2338f2416bd1f4bb15e28ec9bfd37
Henry Wang <henry.h.wang93@gmail.com>
2020-07-31 01:51:29 -0400

    add post data to main app state

--- root/front-end/src/App.js
+++ root/front-end/src/App.js
@@ -10,18 +10,37 @@ class App extends React.Component {
     this.state = {
       username: '',
       proPic: '',
+      postImage: '',
+      postLikes: 0,
+      postDislikes: 0,
+      postDesc: '',
     };
   }
 
   componentDidMount() {
-    console.log('componentDidMount entered!');
+    // console.log('componentDidMount entered!');
     axios
       .get('http://localhost:3000/users')
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         this.setState({
           username: res.data[0].username,
           proPic: res.data[0].profilePic,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get('http://localhost:3000/posts')
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          postImage: res.data[0].postImage,
+          postLikes: res.data[0].postLikes,
+          postDislikes: res.data[0].postDislikes,
+          postDesc: res.data[0].postDesc,
         });
       })
       .catch((err) => {

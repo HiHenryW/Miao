c70369c10a1e737937ce1add49d2f0f8152b25e6
Henry Wang <henry.h.wang93@gmail.com>
2020-07-31 03:23:40 -0400

    add reactions to post

--- root/front-end/src/Components/postreactions.js
+++ root/front-end/src/Components/postreactions.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { GiMeat } from 'react-icons/gi';
+import { FaPoop } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 function Postreactions(props) {
-  return <div>Postreactions placeholder</div>;
+  return (
+    <div className="reactions-container">
+      <span className="likes-container">
+        <IconContext.Provider value={{ color: 'blue', size: '24px' }}>
+          <GiMeat />
+          {props.postLikes}
+        </IconContext.Provider>
+      </span>
+      <span className="dislikes-container">
+        <IconContext.Provider value={{ color: 'brown', size: '15px' }}>
+          <FaPoop />
+          {props.postDislikes}
+        </IconContext.Provider>
+      </span>
+    </div>
+  );
 }
 
 export default Postreactions;

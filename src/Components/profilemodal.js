54390772fa10f541967e43f0037c413e71a8367c
Henry Wang <henry.h.wang93@gmail.com>
2020-08-17 17:36:18 -0400

    change Md to lowercase

--- src/Components/profilemodal.js
+++ src/Components/profilemodal.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { MdClose } from 'react-icons/Md';
+import { MdClose } from 'react-icons/md';
 import { IconContext } from 'react-icons';
 import Commentform from './commentform';
 import Postheader from './postheader';
